95c535404ed4e3b8735166621578223f1fcc2204
Joseph Scheidt <jscheidt@avioranalytics.net>
2020-07-23 15:47:34 -0500

    containerize

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,15 @@
+# To build docker image:
+# docker build --rm -f Dockerfile -t jscheidt/avior:backend .
+
 FROM node:8.11-alpine
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-COPY . .
+# COPY . .
+COPY package*.json ./
 RUN npm install
-ENV PORT=443
-EXPOSE 443
+COPY . .
+ENV PORT=80
+EXPOSE 80
 
 ENV NAME NODE_ENV production
 ENV AWS_REGION us-east-1
--- config/awsconfig.js
+++ config/awsconfig.js
@@ -3,12 +3,12 @@ module.exports = new config();
 function config() {
     
     this.S3BUCKETNAME = 'avior';
-    this.accessKeyId = '******************';
-    this.secretAccessKey = '*******************************';
-    this.S3BASEURL = '*************************';
-    this.cognitoClientId = '***********************';
+    this.accessKeyId = 'AKIAITPBWVGWCRN2O6PA';
+    this.secretAccessKey = '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';
+    this.S3BASEURL = 'https://s3.amazonaws.com/avior/';
+    this.cognitoClientId = '2jbd9o57cefusqfbh1i3idfa2';
     this.region = 'us-east-1';
-    this.userPoolId =  'us-east-************';
-    this.cognitoPublicKeyURL = 'https://cognito-idp.' + 'us-east-1' + '.amazonaws.com/' + 'us-east-*********' + '/.well-known/jwks.json';
+    this.userPoolId =  'us-east-1_aGBWAn8G9';
+    this.cognitoPublicKeyURL = 'https://cognito-idp.' + 'us-east-1' + '.amazonaws.com/' + 'us-east-1_aGBWAn8G9' + '/.well-known/jwks.json';
     
 }
